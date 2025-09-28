c45f8e72498b0a4904f89f3fea7dece964fe8284
Joshua Wise <joshua@joshuawise.com>
2025-09-28 02:59:22 -0400

    put silk-3v7 binary in the right place

--- patch_all.sh
+++ patch_all.sh
@@ -7,7 +7,7 @@ for DEV in silk snowy_dvt snowy_s3 spalding; do
 	./patchpbz.py -v v$NEW_VERSION -b Pebble-$OLD_VERSION-$DEV.pbz out/Pebble-$NEW_VERSION-$DEV.pbz -t tzdata.bin.reso --license rbl-license.txt
 done
 
-./patchpbz.py -v v$NEW_VERSION-3v7 -b Pebble-4.3-silk.pbz Pebble-$NEW_VERSION-silk-3v7.pbz -t tzdata.bin.reso --license rbl-license.txt --silk-3v7
+./patchpbz.py -v v$NEW_VERSION-3v7 -b Pebble-4.3-silk.pbz out/Pebble-$NEW_VERSION-silk-3v7.pbz -t tzdata.bin.reso --license rbl-license.txt --silk-3v7
 
 NEW_VERSION_V3=3.13.1-rbl
 OLD_VERSION_V3=3.12.3
