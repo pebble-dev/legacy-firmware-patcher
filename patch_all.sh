12d0c4e80697e51956ca4c849dfab18b12c1c705
Joshua Wise <joshua@joshuawise.com>
2025-09-28 02:56:06 -0400

    add patch_all with 3.13.1

---
+++ patch_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+NEW_VERSION=4.4.1-rbl
+OLD_VERSION=4.3
+
+for DEV in silk snowy_dvt snowy_s3 spalding; do
+	./patchpbz.py -v v$NEW_VERSION -b Pebble-$OLD_VERSION-$DEV.pbz out/Pebble-$NEW_VERSION-$DEV.pbz -t tzdata.bin.reso --license rbl-license.txt
+done
+
+./patchpbz.py -v v$NEW_VERSION-3v7 -b Pebble-4.3-silk.pbz Pebble-$NEW_VERSION-silk-3v7.pbz -t tzdata.bin.reso --license rbl-license.txt --silk-3v7
+
+NEW_VERSION_V3=3.13.1-rbl
+OLD_VERSION_V3=3.12.3
+
+for DEV in v2_0 v1_5 ev2_4; do
+	./patchpbz.py -v v$NEW_VERSION_V3 -b Pebble-$OLD_VERSION_V3-$DEV.pbz out/Pebble-$NEW_VERSION_V3-$DEV.pbz -t tzdata.bin.reso --license rbl-license.txt
+done
